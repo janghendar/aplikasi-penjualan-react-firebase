45c163f734d85258279202b9922fe89069be7aa5
Suhendar Aryadi <suhendarweb@gmail>
2020-08-15 02:46:12 +0700

    Add Route and Firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //Komponen Halaman
 import Registrasi from "./pages/registrasi";
@@ -10,22 +10,28 @@ import NotFound from './pages/404';
 import Private from './pages/private';
 import PrivateRoute from './components/PrivateRoute';
 
+// Firebase Provider
+import FirebaseProvider from './components/FirebaseProvider';
+
 function App() {
 
   return (
-    <Router>
-      <Switch>
-        <PrivateRoute path="/" exact component={Private} />
-        <PrivateRoute path="/pengaturan" component={Private}/>
-        <PrivateRoute path="/produk" component={Private}/>
-        <PrivateRoute path="/transaksi" component={Private} />
-        <Route path="/registrasi" component={Registrasi} />
-        <Route path="/login" component={Login}/>
-        <Route path="/lupa-password" component={LupaPassword}/>
-        <Route component={NotFound}/>
-      </Switch>
-    </Router>
-    
+
+    <FirebaseProvider>
+      <Router>
+        <Switch>
+          <PrivateRoute path="/" exact component={Private} />
+          <PrivateRoute path="/pengaturan" component={Private} />
+          <PrivateRoute path="/produk" component={Private} />
+          <PrivateRoute path="/transaksi" component={Private} />
+          <Route path="/registrasi" component={Registrasi} />
+          <Route path="/login" component={Login} />
+          <Route path="/lupa-password" component={LupaPassword} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </FirebaseProvider>
+
   );
 }
 
---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAFSgCKwrk1z3YQq2UekimzTp2qlriIozg",
+    authDomain: "corded-keel-285616.firebaseapp.com",
+    databaseURL: "https://corded-keel-285616.firebaseio.com",
+    projectId: "corded-keel-285616",
+    storageBucket: "corded-keel-285616.appspot.com",
+    messagingSenderId: "839081243869",
+    appId: "1:839081243869:web:66a0d4e2ea8c93cacf9734",
+    measurementId: "G-8TH495QML2"
+  };
+
+  export default firebaseConfig;
