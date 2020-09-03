77fc32440e712e9fbc504689f2eaa7378d071a73
janghendar <suhendarweb@gmail.com>
2020-09-03 16:05:10 +0700

    Menyelesaikan Halaman registrasi

--- src/pages/registrasi/index.js
+++ src/pages/registrasi/index.js
@@ -7,6 +7,8 @@ import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
+import isEmail from 'validator/lib/isEmail';
+
 
 // Import Style
 import useStyles from "./styles";
@@ -14,31 +16,100 @@ import useStyles from "./styles";
 // Import dari React Dom
 import { Link } from 'react-router-dom';
 
+// firebase Hook
+import {useFirebase} from '../../components/FirebaseProvider';
+
 
 function Registrasi() {
 
     const classes = useStyles();
     const [form, setForm] = useState({
-        email:'',
-        password:'',
-        ulangi_password:'',
+        email: '',
+        password: '',
+        ulangi_password: '',
     });
 
-    const handleChange = e=>{
+    const [error, setError] = useState({
+        email: '',
+        password: '',
+        ulangi_password: ''
+    })
+
+    const {auth} = useFirebase();
+
+    const handleChange = e => {
         setForm({
             ...form,
-            [e.target.name]:e.target.value
+            [e.target.name]: e.target.value
+        })
+        setError({
+            ...error,
+            [e.target.name]:''
         })
     }
 
+    const validate = () => {
+        const newError = { ...error };
+
+        if (!form.email) {
+            newError.email = 'Email Wajib di Isi';
+        } else if (!isEmail(form.email)) {
+            newError.email = 'Email Tidak Valid';
+        }
+
+        if (!form.password) {
+            newError.password = 'Password wajib diisi';
+        }
+
+        if (!form.ulangi_password) {
+            newError.ulangi_password = 'Ulangi Password Wajib diisi';
+        } else if (form.ulangi_password !== form.password) {
+            newError.ulangi_password = 'Ulangi password tidak sama dengan password';
+        }
+
+        return newError;
+
+
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const findErrors = validate();
+
+        if (Object.keys(findErrors).some(err => err !== '')) {
+            setError(findErrors);
+        }else{
+            try{
+
+                await
+                auth.createUserWithEmailAndPassword(form.email,form.password)
+            }catch(e){
+                const newError = {};
+
+                switch(e.code){
+                    case 'auth/email-aready-in-use' :
+                        newError.email = 'Email sudah terdaftar'
+                        break;
+                    case 'auth/invalid-email' :
+                        newError.email = 'email tidak valid';
+                        break;
+                    case 'auth/weak-password' :
+                        newError.password = 'passsword anda terlalu lemah'
+                }
+            }
+
+        }
+
+    }
+    console.log(form)
     return <Container maxWidth="xs">
         <Paper className={classes.paper}>
-            <Typography 
-            variant="h5" 
-            component="h1" 
-            className={classes.title}>Buat Akun Baru</Typography>
+            <Typography
+                variant="h5"
+                component="h1"
+                className={classes.title}>Buat Akun Baru</Typography>
 
-            <form noValidate>
+            <form onSubmit={handleSubmit} noValidate>
                 <TextField
                     id="email"
                     type="email"
@@ -49,7 +120,10 @@ function Registrasi() {
                     required
                     value={form.email}
                     onChange={handleChange}
-                
+                    helperText={error.email}
+                    error={error.email?true:false}
+
+
                 />
                 <TextField
                     id="password"
@@ -61,9 +135,11 @@ function Registrasi() {
                     required
                     value={form.password}
                     onChange={handleChange}
-                
+                    helperText={error.password}
+                    error={error.password?true:false}
+
                 />
-                 <TextField
+                <TextField
                     id="ulangi_password"
                     type="password"
                     name="ulangi_password"
@@ -73,34 +149,36 @@ function Registrasi() {
                     required
                     value={form.ulangi_password}
                     onChange={handleChange}
-                
+                    helperText={error.ulangi_password}
+                    error={error.ulangi_password?true:false}
+
                 />
 
                 <Grid container className={classes.buttons}>
                     <Grid item xs>
-                        <Button 
-                        type="submit"
-                        color="primary"
-                        variant="contained"
-                        size="large">
+                        <Button
+                            type="submit"
+                            color="primary"
+                            variant="contained"
+                            size="large">
                             Daftar
                         </Button>
                     </Grid>
 
                     <Grid>
                         <Button
-                        component={Link}
-                        variant="contained"
-                        size="large"
-                        to="/login">
+                            component={Link}
+                            variant="contained"
+                            size="large"
+                            to="/login">
                             Login
                         </Button>
                     </Grid>
 
                 </Grid>
             </form>
-            
-            
+
+
         </Paper>
     </Container>
 }
